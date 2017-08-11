1215122bb2ff2363019680fb3959fc94ca56ba87
jlo755 <jlo755@aucklanduni.ac.nz>
2017-08-12 01:26:54 +1200

    Fixed merge conflict of the InputParser File

--- src/InputParser.java
+++ src/InputParser.java
@@ -25,44 +25,52 @@ public class InputParser {
 		System.out.println("hello");
 		bfr.readLine(); // get rid of the first line in the input
 
-		while (!lineToRead.equals("}")) { // if we havent reached the end of the input which ends with "}"
-			lineToRead = bfr.readLine(); // get the line
-			lineToRead = lineToRead.replaceAll("\\s", ""); // remove all the spaces
-			// get rid of the first line in the input
-			bfr.readLine();
 			// if we havent reached the end of the input which ends with "}"
 			while (!lineToRead.equals("}")) {
+				
 				// get the line
 				lineToRead = bfr.readLine();
 				// remove all the spaces
 				lineToRead = lineToRead.replaceAll("\\s", "");
-				int i = 0;
-				// when there is an arrow in the line
+				
+				// when there is an arrow in the line, it describes dependencies between two tasks
 				if (lineToRead.contains("->")) {
+					
 					// getting position of the left-side of arrow to get the parent name of node
 					int IndexOfArrowHead = lineToRead.indexOf("-");
+					
 					// getting position of the point of arrow to appropriately get the child name of
 					// node
 					int IndexOfArrowEnd = lineToRead.indexOf(">");
+					
 					// determining the end position of the child node's name
 					int IndexOfWeight = lineToRead.indexOf("[");
 					String nameOfParentNode = lineToRead.substring(0, IndexOfArrowHead);
 					String nameOfChildrenNode = lineToRead.substring(IndexOfArrowEnd + 1, IndexOfWeight);
+					
 					// determining position of when the weight begins
 					int indexOfStartCost = lineToRead.indexOf("=");
 					int weight = Integer.parseInt(lineToRead.substring(indexOfStartCost + 1, lineToRead.length() - 2));
+					
+					// 
 					Node parentNode = nodeMap.get(nameOfParentNode);
 					Node childNode = nodeMap.get(nameOfChildrenNode);
 					nodeMap.get(nameOfParentNode).addChild(childNode, weight);
 					nodeMap.get(nameOfChildrenNode).addParent(parentNode, weight);
-					// when there is no arrow in the line
+					
+					// when there is no arrow in the line, it indicates a new task being introduced
 				} else if (!lineToRead.equals("}")) {
+					
+					// get start of the node name
 					int IndexOfWeight = lineToRead.indexOf("[");
+					
 					// everything before the "]" is the name of the node
 					String nameOfNode = lineToRead.substring(0, IndexOfWeight);
 					int indexOfStartCost = lineToRead.indexOf("=");
+					
 					// everything after the equals is the weight, - 2 to get rid of the " ] ;"
 					int weight = Integer.parseInt(lineToRead.substring(indexOfStartCost + 1, lineToRead.length() - 2));
+					
 					// hashmap which has for node a eg: (a, node a) as the input
 					nodeMap.put(nameOfNode, new Node(nameOfNode, weight));
 				}
