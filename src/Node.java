a34145d9f591c5f418c79cfd72a538df7ca843d0
jlo755 <jlo755@aucklanduni.ac.nz>
2017-08-12 14:24:46 +1200

    Made Node into an Abstract Class.

--- src/Node.java
+++ src/Node.java
@@ -1,39 +1,17 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Node {
-	private HashMap<Node, Double> _parents; // this is the parents hashmap with the parent node and their communication cost
-	private HashMap<Node, Double>_children; // this is the children hashmap with the child node and their comunication cost
+public abstract class Node {
+	protected HashMap<Node, Double>_children; // this is the children hashmap with the child node and their comunication cost
 	private String _id; // name of the node
 	private int _cost; // cost of the node itself
-	private HashMap<String, Double> _distances;
 	private boolean _completed;
-	private int _processor;
-	private double _startTime;
-	private double _endTime;
-	private ArrayList<Node> _stateParents;
 
 	protected Node(String id, int cost){
 		_id = id;
 		_cost = cost;
-		_parents = new HashMap<Node, Double>();
 		_children = new HashMap<Node, Double>();
-		_distances = new HashMap<String, Double>(); // storing the distances in a matrix to all the other nodes in the network (infinitiy and actual reachable costs) 
 		_completed = false;
 		// stores all the nodes we visited in the state space before this node was visited
-		_stateParents = new ArrayList<Node>();
-	}
-
-	protected void addParent(Node node, double cost){
-		_parents.put(node, cost);
-	}
-	
-	protected void addStateParents(Node node){
-		_stateParents.add(node);
-	}
-	
-	protected ArrayList<Node> getStateParents(){
-		return _stateParents;
 	}
 
 	protected void addChild(Node node, double cost){
@@ -44,30 +22,6 @@ public class Node {
 		return _completed;
 	}
 
-	public void setProcessor(int i){
-		_processor = i;
-	}
-
-	public int getProcessor(){
-		return _processor;
-	}
-
-	public void setStartTime(Double i){
-		_startTime = i;
-	}
-
-	public double getStartTime(){
-		return _startTime;
-	}
-
-	public void setEndTime(Double i){
-		_endTime = i;
-	}
-
-	public double getEndTime(){
-		return _endTime;
-	}
-
 	public void setCompleted(boolean completed) {
 		_completed = completed;
 	}
@@ -76,20 +30,8 @@ public class Node {
 		return _id;
 	}
 
-	protected void updateDistance(String n, double i){
-		_distances.put(n, i);
-	}
-
-	protected HashMap<String, Double> getDistances(){
-		return _distances;
-	}
-
 	protected int getCost(){
 		return _cost;
-	}
-
-	protected HashMap<Node, Double> getParents(){
-		return _parents;
 	}
 
 	protected HashMap<Node, Double> getChildren(){
