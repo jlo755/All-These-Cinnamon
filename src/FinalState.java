c268f803356e1551829ddfe06822a5976627253b
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-13 18:05:55 +1200

    Added JavaDocs to the FinalState class.

--- src/FinalState.java
+++ src/FinalState.java
@@ -2,9 +2,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
+import DataStructure.Node;
+
+/**
+ * 
+ * This class stores and manages the currently found "best solution" for a Node object,
+ * used while traversing state space. A separate Node object is used to track this. 
+ *
+ */
 public class FinalState {
+	// Current best solution for a Node mapped to an ID in String.
 	HashMap<String, Node> _currentBestState;
 	
+	/**
+	 * Instantiate a set of Nodes' best solution to be 0.
+	 * @param set
+	 */
 	public FinalState(Set<String> set){
 		_currentBestState = new HashMap<String, Node>();
 		for(String s: set){
@@ -13,6 +26,10 @@ public class FinalState {
 
 	}
 	
+	/**
+	 * Set the current best state for all the Nodes in a graph.
+	 * @param graph
+	 */
 	public void setCurrentBestState(HashMap<String, Node> graph){
 		for(String nodeID : graph.keySet()){
 			Node n = _currentBestState.get(nodeID);
@@ -23,10 +40,17 @@ public class FinalState {
 		}
 	}
 	
+	/**
+	 * Get HashMap for current best state of Nodes.
+	 * @return
+	 */
 	public HashMap<String, Node> getCurrentBestState(){
 		return _currentBestState;
 	}
 	
+	/**
+	 * Print current best state of all Nodes.
+	 */
 	public void printCurrentBestState(){
 		for(String s:_currentBestState.keySet()){
 			Node n = _currentBestState.get(s);
