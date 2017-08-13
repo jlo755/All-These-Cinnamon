b42a79d3991183c57555cd28854ee3a49a58a54e
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-13 23:53:18 +1200

    Renamed RecursiveDFS to LaunchScheduler

--- src/LaunchScheduler.java
+++ src/LaunchScheduler.java
@@ -17,20 +17,16 @@ import OutputParse.OutputParser;
 * placed on the different processors. We then check which of these is the best time. This process continues until all the nodes
 * in the network are visited and we have obtained the best scheduling times.
 */
-public class RecursiveDFS {
+public class LaunchScheduler {
 
 	public static void main(String[] args) throws IOException, ImportException {
 		
 		Scheduler scheduler = new Scheduler();
 
-		//InputParser parser = new InputParser();
-		//parser.parseInput(nodeMap);
 		DotParser dotParser = new DotParser("Nodes_7_OutTree.dot");
 		dotParser.parseInput();
 		scheduler.provideTaskGraph(dotParser.getNodeMap());
-		//scheduler.setBestState(new FinalState(nodeMap.keySet()));
 		scheduler.schedule();
-		
 		
 		OutputParser outputParse = new OutputParser();
 		DirectedAcyclicGraph<Node, Edge> graph = dotParser.getGraph();
