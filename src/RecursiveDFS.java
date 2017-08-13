d0759084bc351dcee2226dbd120a73510c1b9bdc
sindhusingamneni <sindhusingamneni@gmail.com>
2017-08-13 18:32:53 +1200

    Added in the Javadoc commenting and overall comments
    Added in the Javadoc commenting for the overall class as well as methods which didn't have any Javadoc commenting to begin with. Also added in some overall comments to the methods

--- src/RecursiveDFS.java
+++ src/RecursiveDFS.java
@@ -2,7 +2,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
-
+/**
+* This class recursively calls the recursive method to get the children nodes of a particular node. As this occurs recursively,
+* we traverse through the entire graph. When a node is visited, we check all possible time costs for that particular node being
+* placed on the different processors. We then check which of these is the best time. This process continues until all the nodes
+* in the network are visited and we have obtained the best scheduling times.
+*/
 public class RecursiveDFS {
 
 	static double currentBestSolution = Double.POSITIVE_INFINITY;
@@ -10,15 +15,18 @@ public class RecursiveDFS {
 
 	public static void main(String[] args) throws IOException {
 		HashMap<String, Node> nodeMap = new HashMap<String, Node>();
-
+		
 		InputParser parser = new InputParser();
 		parser.parseInput(nodeMap);
 		bestState = new FinalState(nodeMap.keySet());
+		// "Nodemap" is the input graph for the algorithm.
 		for(int i = 1; i<=1; i++) {
 			for(Node n:nodeMap.values()) {
+				// If a node doesn't not have parents, it is a starting node
 				if(n.getParents().isEmpty()) {
 					n.setProcessor(i);
 					long startTime = System.nanoTime();
+					// call the resursive DFS method for that node to obtain and process all of the children
 					recursiveDFS(nodeMap, n.getID());
 					long endTime = System.nanoTime();
 					System.out.println((endTime-startTime)/ 1000000000.0);
@@ -35,20 +43,29 @@ public class RecursiveDFS {
 	 * space of the scheduling problem.
 	 * 
 	 *
-	 * @param nodeMap
-	 * @param initialNode
+	 * @param graph This is the input graph
+	 * @param nodeName This is the node which we are starting off from
 	 */
 
 	public static void recursiveDFS(HashMap<String, Node> graph, String nodeName) {
+		// set the node as being completed
 		graph.get(nodeName).setCompleted(true);
 		ArrayList<String> test = getReachable(graph);
+		// this calculates the time at which the node task can be placed in that particular processor
 		calculateTime(graph, graph.get(nodeName));
+		// loop through the different available processors to check for and compare between the final times when the node
+		// is placed on each of the processors
+		
 		for(int i = 1; i<=2; i++) {
+			// Repeat these processes until all of the processor and node possibilities have been covered
 			for(String node1:test) {
 				graph.get(node1).setProcessor(i);
+				// call the recursive DFS method to check for the processing times of the child nodes
 				recursiveDFS(graph, node1);
+				// set the times of the node to 0
 				graph.get(node1).setStartTime(0.0);
 				graph.get(node1).setEndTime(0.0);
+				// set the node as not finished
 				graph.get(node1).setCompleted(false);
 			}
 			if(test.isEmpty()) {
@@ -58,6 +75,7 @@ public class RecursiveDFS {
 						max = n.getEndTime();
 					}
 				}
+				// reset the best solution if a lower scheduling time has been found
 				if(max < currentBestSolution) {
 					currentBestSolution = max;
 					bestState.setCurrentBestState(graph);
@@ -69,9 +87,8 @@ public class RecursiveDFS {
 
 	/**
 	 * Get a list of reachable Nodes from the supplied Node in a given graph.
-	 * @param graph
-	 * @param currentNode
-	 * @return
+	 * @param graph This is the input node graph
+	 * @return reachableNodeNames the reachable nodes list
 	 */
 	public static ArrayList<String> getReachable(HashMap<String, Node> graph) {
 		ArrayList<String> reachableNodeNames = new ArrayList<String>();
@@ -80,12 +97,14 @@ public class RecursiveDFS {
 				continue;
 			}
 			boolean dependenciesDone = true;
+			// if the parent nodes have not been completed, then the dependencies are not complete
 			for(Node parentNode:graph.get(nodes).getParents().keySet()){
 				if(!parentNode.getCompleted()){
 					dependenciesDone = false;
 					break;
 				}
 			}
+			// add the node into the list as reachable if the dependencies are done
 			if(dependenciesDone){
 				reachableNodeNames.add(nodes);
 			}
@@ -94,14 +113,13 @@ public class RecursiveDFS {
 	}
 
 	/**
-	 * @Param g - is the original dependency graph, NOT the state space. In our
+	 * This calculates the time at which the node can be placed in the scheduler in that particular processor
+	 * @param graph - is the original dependency graph, NOT the state space. In our
 	 *        case, it stores HashMap<String, Node> - name of the node, to the Node
 	 *        itself.
 	 * 
-	 * @Param node - this is the current node we’re calculating time for.
+	 * @param node - this is the current node we’re calculating time for.
 	 * 
-	 * @Param list - stores the name of the processor, ie processor1 and the nodes
-	 *        on that processor.
 	 **/
 
 	public static void calculateTime(HashMap<String, Node> graph, Node node){
@@ -140,14 +158,24 @@ public class RecursiveDFS {
 		}
 	}
 
+	/**
+	 * Checks if a node has parents in a processor and returns true or false
+	 * @param graph  is the original dependency graph, NOT the state space. In our
+	 *              case, it stores HashMap<String, Node> - name of the node, to the Node
+	 *              itself.
+	 * @param node this is the current node we’re checking the parent for
+	 * @return returns whether the node has a parent in a processor
+	*/
 
 	public static boolean checkListContainsParent(HashMap<String, Node> graph, Node node){
 		ArrayList<String> completedNodes = new ArrayList<String>();
+		// populate the completed nodes list according to whether it is set as completed
 		for(Node n: graph.values()) {
 			if(n.getProcessor() == node.getProcessor() && n.getCompleted()) {
 				completedNodes.add(n.getID());
 			}
 		}
+		// get all the parents of that node and check if they are present in the processor
 		for(Node n : node.getParents().keySet()){
 			if(n.getProcessor() != node.getProcessor()){
 				return true;
@@ -159,11 +187,19 @@ public class RecursiveDFS {
 		return false;
 	}
 
-//gets last node in the same processor
+	/**
+	* This method returns the max time for a processor
+	* @param graph This is the graph containing all the nodes in the network
+	* @param processor This is the processor we are obtaining the max time for
+	* @return This returns the mac time for that particular processor
+	*/
 	public static double calculateMaxCurrentProcessorTime(HashMap<String, Node> graph, int processor){
 		double max = 0.0;
+		// go through the graph and then obtain the maximum time in the schedule for that particular processor
 		for(Node n : graph.values()){
+			// check if the node exists in the processor we are trying to get the max time for
 			if(n.getProcessor() == processor) {
+				// update the max time
 				if (n.getEndTime() > max) {
 					max = n.getEndTime();
 				}
@@ -172,7 +208,12 @@ public class RecursiveDFS {
 		return max;
 	}
 
-
+	/**
+	* This method calculates the commucation cost between a node and its parent
+	* @param node This is the node we are trying to get the communication cost for
+	* @param parent This is the parent node
+	* @return This returns the cost between the node and its parent. If the link doesn't exist, this returns 0.0
+	*/
 	public static double calculateCommunicationCost(Node node, Node parent){
 		// get children list of parent
 		for(Node n1: parent.getChildren().keySet()){
@@ -185,6 +226,12 @@ public class RecursiveDFS {
 		return 0.0;
 	}
 
+	/**
+	* This method gets the communication cost between this node and its parents and then adds it to the
+	* task processing cost of the node and then returns this total cost
+	* @param node This is the node we are trying to get the maximum communication cost for
+	* @return This returns the cost which includes the communication cost and the task cost
+	*/
 	public static double getMaxCommunicationCost(Node n){
 		double ccost=0.0;
 		//get parent list of a node, where parent belongs to a different processor
