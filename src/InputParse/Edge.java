bc8d90f2964af2c65c776352021cc9c1eec28ad0
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-14 16:15:08 +1200

    Added JavaDocs to Edge class.

--- src/InputParse/Edge.java
+++ src/InputParse/Edge.java
@@ -2,16 +2,33 @@ package InputParse;
 
 import DataStructure.Node;
 
+/**
+ * 
+ * Class representing an edge between a parent and a child
+ * node within a graph, and its cost.
+ *
+ */
 public class Edge {
 	private Node _childNode;
 	private Node _parentNode;
 	private double _cost;
 	
+	/**
+	 * Initialize an edge between two nodes.
+	 * @param childNode
+	 * @param parentNode
+	 */
 	public Edge(Object childNode, Object parentNode){
 		_childNode = (Node) childNode;
 		_parentNode = (Node) parentNode;
 	}
 	
+	/**
+	 * Initialize an edge with cost between two nodes.
+	 * @param childNode
+	 * @param parentNode
+	 * @param cost
+	 */
 	public Edge(Object childNode, Object parentNode, double cost){
 		_childNode = (Node) childNode;
 		_parentNode = (Node) parentNode;
@@ -20,6 +37,10 @@ public class Edge {
 		_cost = cost;
 	}
 	
+	/**
+	 * Return the cost of the edge.
+	 * @return
+	 */
 	public double getCost() {
 		return _cost;
 	}
