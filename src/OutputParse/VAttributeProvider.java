8643cfa1312a951f6567338cbca87f5d59954190
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-14 19:53:47 +1200

    JavaDoc and comments for VAttributeProvider.

--- src/OutputParse/VAttributeProvider.java
+++ src/OutputParse/VAttributeProvider.java
@@ -7,6 +7,11 @@ import org.jgrapht.ext.ComponentAttributeProvider;
 
 import DataStructure.Node;
 
+/**
+ * Implements VAttributeProvider to get the weight, start time and processor number for
+ * a specific node.
+ *
+ */
 public class VAttributeProvider implements ComponentAttributeProvider<Node>{
 
 	@Override
@@ -15,6 +20,7 @@ public class VAttributeProvider implements ComponentAttributeProvider<Node>{
 		int NodeStart = (int)node.getStartTime();
 		int NodeProcessor = node.getProcessor();
 		
+		// LinkedHashMap keeps track of ordering for correct output.
 		Map<String, String> map = new LinkedHashMap<String, String>();
 		map.put("Weight", NodeCost+",");
 		map.put("Start", NodeStart+",");
