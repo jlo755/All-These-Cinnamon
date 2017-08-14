4e384837d2533981ba5fdb68ca493bc29641f17f
Shibani T <shibani.timblo@gmail.com>
2017-08-15 11:27:23 +1200

    added comments to eProvider
    added in buildEdge

--- src/inputParse/eProvider.java
+++ src/inputParse/eProvider.java
@@ -14,6 +14,8 @@ public class eProvider implements EdgeProvider<Node, Edge>{
 
 	@Override
 	public Edge buildEdge(Node parentNode, Node childNode, String arg2, Map<String, String> arg3) {
+		// reads and parses the weight attribute from the .DOT file
+		// ascribes that attribute to the link between the current vertex and its parent
 		return new Edge(childNode, parentNode, Double.parseDouble((String) arg3.get("Weight")));
 	}
 	
