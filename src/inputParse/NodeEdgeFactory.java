688321d2b50c4108b67f9d087aeaf729be8c544f
Shibani T <shibani.timblo@gmail.com>
2017-08-20 19:03:24 +1200

    Add NodeEdgeFactory file

---
+++ src/inputParse/NodeEdgeFactory.java
@@ -0,0 +1,21 @@
+package inputParse;
+import org.jgrapht.EdgeFactory;
+
+import dataStructure.Node;
+
+
+/**
+ * 
+ * Implements EdgeFactory to return an Edge object to build the graph.
+ *
+ */
+
+public class NodeEdgeFactory implements EdgeFactory<Node, Edge> {
+
+
+	@Override
+	public Edge createEdge(Node childNode, Node parentNode) {
+		return new Edge(childNode, parentNode);
+	}
+
+}
