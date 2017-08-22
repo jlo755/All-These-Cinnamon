9fd9d175931f4a267204aeee095e7de0cb0361f1
Shibani T <shibani.timblo@gmail.com>
2017-08-22 15:48:17 +1200

    Trying to add attributes to Nodes
    attributes display data on the graph

--- src/outputGraph/VisualGraph.java
+++ src/outputGraph/VisualGraph.java
@@ -1,8 +1,10 @@
-package graphOutput;
+package outputGraph;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
 
 import java.util.HashMap;
 
@@ -13,13 +15,18 @@ public class VisualGraph {
 
     public VisualGraph(HashMap<String, dataStructure.Node> g){
         Graph graph = new SingleGraph("MainGraph");
+        SpriteManager sm = new SpriteManager(graph);
+        Sprite s = sm.addSprite("S1");
         graph.setStrict(false);
         graph.setAutoCreate( true );
         for(dataStructure.Node node : g.values()) {
+            s.attachToNode(""+node.getID());
             for (dataStructure.Node parent : node.getParents().keySet()) {
                 graph.addEdge("" + node.getID() + parent.getID(), "" + node.getID(), "" + parent.getID());
+                s.attachToNode(""+parent.getID());
             }
         }
         graph.display();
+
     }
 }
