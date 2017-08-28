d8add105d1aa24ae9232c5e55d9174ad678011cf
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-28 16:55:52 +1200

    JavaDoc for ScheduleListener

--- src/scheduling/ScheduleListener.java
+++ src/scheduling/ScheduleListener.java
@@ -1,5 +1,9 @@
 package scheduling;
 
+/**
+ * Interface for a listener that updates upon being notified of a change in the optimal scheduling calculations.
+ *
+ */
 public interface ScheduleListener {
 	public void update();
 }
